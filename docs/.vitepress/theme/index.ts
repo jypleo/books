e7bd15d52ddea7ad87013f0338651445fe1aefe9
jiangyp <jiangyp@chinawyny.com>
2024-08-01 10:56:21 +0800

    feat: 增加图片缩放插件

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,26 @@
+// .vitepress/theme/index.ts
+import DefaultTheme from 'vitepress/theme'
+
+import mediumZoom from 'medium-zoom';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+import './style/var.css';
+
+export default {
+  ...DefaultTheme,
+
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom()),
+    );
+  },
+};
